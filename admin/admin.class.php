a99345704a8c26828dde9780dcf2d031c270c2ed
optimalisatie <optimalisatie@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-25 09:00:47 +0000

    spelling
    git-svn-id: https://plugins.svn.wordpress.org/above-the-fold-optimization/trunk@1166950 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.class.php
+++ admin/admin.class.php
@@ -295,7 +295,7 @@ class Abovethefold_Admin {
 		<div id="post-body-content" style="padding-bottom:0px;margin-bottom:0px;">
 			<div class="postbox">
 				<div class="inside" style="margin:0px;">
-					<p>Developed by <strong><a href="https://en.optimalisatie.nl/#utm_source=wordpress&utm_medium=link&utm_term=optimization&utm_campaign=Above%20the%20fold" target="_blank">Optimalisatie.nl</a></strong> - Website Optimization and Internationalisation
+					<p>Developed by <strong><a href="https://en.optimalisatie.nl/#utm_source=wordpress&utm_medium=link&utm_term=optimization&utm_campaign=Above%20the%20fold" target="_blank">Optimalisatie.nl</a></strong> - Website Optimization and Internationalization
 					<br />Contribute via <a href="https://github.com/optimalisatie/wordpress-above-the-fold-optimization" target="_blank">Github</a> &dash; <a href="https://github.com/optimalisatie/wordpress-above-the-fold-optimization/issues" target="_blank">Report a bug</a> &dash; <a href="https://wordpress.org/support/view/plugin-reviews/above-the-fold-optimization?rate=5" target="_blank">Review this plugin</a></p>
 				</div>
 			</div>
